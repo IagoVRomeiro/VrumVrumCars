4c5124871c4363c7cbe25b35e1a6a8422c96da9f
IagoVRomeiro <150415909+IagoVRomeiro@users.noreply.github.com>
2023-12-14 02:13:57 -0300

    trab

--- linhas.h
+++ linhas.h
@@ -4,7 +4,6 @@
 #define MARCAS "marcas.csv"
 #define HISTORICO_VENDAS "historico-vendas.csv"
 
-
 #include "carro.h"
 
 
@@ -36,8 +35,7 @@ void remover_linha_arquivo(char *nome_arquivo, const char *remover_linha) {
     fclose(arquivo_temporario);
 
     remove(nome_arquivo);  
-    rename("temporario.txt", nome_arquivo); 
-}
+    rename("temporario.txt", nome_arquivo); }
 
 void adicionar_linha_arquivo( char *nome_arquivo,  char *adicionar_linha) {
     FILE *arquivo = fopen(nome_arquivo, "a");
@@ -49,6 +47,52 @@ void adicionar_linha_arquivo( char *nome_arquivo,  char *adicionar_linha) {
     fputs(adicionar_linha, arquivo);
     fputs("\n", arquivo);
     fclose(arquivo);
+}
+
+void removerLinhaPorMarca(const char *nomeArquivoEstoque, char *nomeArquivoMarcas) {
+    FILE *estoque = fopen(nomeArquivoEstoque, "r");
+    FILE *marcas = fopen(nomeArquivoMarcas, "r");
+    FILE *temporario = fopen("temp.csv", "w");
+
+    if (estoque == NULL || marcas == NULL || temporario == NULL) {
+        perror("Falha ao abrir arquivo");
+        exit(EXIT_FAILURE);
+    }
+
+    char bufferEstoque[TAMANHO_LINHA];
+    char bufferMarcas[TAMANHO_LINHA];
+
+    while (fgets(bufferMarcas, TAMANHO_LINHA, marcas) != NULL) {
+        char *token = strtok(bufferMarcas, "=");
+        if (token != NULL) {
+            // extrair a marca do arquivo marcas.csv
+            char *marca = strtok(NULL, "=");
+            strtok(marca, " :");
+            
+            // Verificar se a marca está presente no arquivo estoque.csv
+            int marcaEncontrada = 0;
+            rewind(estoque);
+            while (fgets(bufferEstoque, TAMANHO_LINHA, estoque) != NULL) {
+                char *tokenEstoque = strtok(bufferEstoque, ",");
+                tokenEstoque = strtok(NULL, ",");
+                if (tokenEstoque != NULL && strcmp(tokenEstoque, marca) == 0) {
+                    marcaEncontrada = 1;
+                    break;
+                }
+            }
+
+            // Se a marca não for encontrada em estoque.csv, remover a linha
+            if (!marcaEncontrada) {} 
+            else {fputs(bufferMarcas, temporario);}
+        }
+    }
+
+    fclose(estoque);
+    fclose(marcas);
+    fclose(temporario);
+
+    remove(nomeArquivoMarcas);
+    rename("temp.csv", nomeArquivoMarcas);
 }
 
 void marcasAdiciona(char *marca) {
@@ -75,21 +119,106 @@ FILE *arquivoMarcas = fopen(MARCAS, "a+");
         printf("Digite a taxa de venda para a marca %s: ", marca);
         scanf("%f", &taxa);
 
-        fprintf(arquivoMarcas, "%s %.2f\n", marca, taxa);
+        fprintf(arquivoMarcas, "%s = taxa: %.2f\n", marca, taxa);
     }
          fclose(arquivoMarcas);
 }
 
-void obter_data_hora_atual(char *buffer, unsigned int tamanho_buffer) {
-    time_t tempo_registrado;
-    struct tm *tempoinfo;
+void exibirConteudo(FILE *arquivo) {
+    char ch;
+    rewind(arquivo);
 
-    time(&tempo_registrado);
-    tempoinfo = localtime(&tempo_registrado);
+    while ((ch = fgetc(arquivo)) != EOF) {
+        printf("%c", ch);
+    }
 
-    strftime(buffer, tamanho_buffer, "%Y-%m-%d %H:%M:%S", tempoinfo);
+    printf("\n");
 }
 
+void editarArquivoCSV() {
+    char nome_arquivo[TAMANHO_LINHA];
 
+    printf("Escolha o arquivo CSV que deseja editar:\n");
+    printf("1. %s\n", MARCAS);
+    printf("2. %s\n", ESTOQUE);
+    printf("Digite o numero correspondente: ");
 
+    int escolha;
+    scanf("%d", &escolha);
 
+    // Limpar o buffer do teclado
+    while (getchar() != '\n');
+
+    FILE *arquivo;
+
+    switch (escolha) {
+        case 1:
+            strcpy(nome_arquivo, MARCAS);
+            break;
+        case 2:
+            strcpy(nome_arquivo, ESTOQUE);
+            break;
+        default:
+            printf("Escolha invalida. Saindo...\n");
+            return;
+    }
+
+    arquivo = fopen(nome_arquivo, "r+");
+    if (arquivo == NULL) {
+        perror("Falha ao abrir o arquivo");
+        exit(EXIT_FAILURE);
+    }
+
+    // Exibir o conteúdo atual do arquivo
+    printf("Conteudo atual do arquivo %s:\n", nome_arquivo);
+    exibirConteudo(arquivo);
+
+    // Permitir ao usuário editar o conteúdo
+    printf("Edite o conteudo. Pressione Ctrl+D (ou Ctrl+Z no Windows) para encerrar a ediçao:\n");
+
+    char buffer[TAMANHO_LINHA];
+    fseek(arquivo, 0, SEEK_SET); // Voltar ao início do arquivo antes da edição
+
+    while (fgets(buffer, TAMANHO_LINHA, stdin) != NULL) {
+        fputs(buffer, arquivo);
+    }
+
+    // Solicitar ao usuário se deseja salvar as alterações
+    printf("Pressione Enter para salvar as alteraçoes, ou qualquer outra tecla para sair sem salvar: ");
+    char salvar;
+    scanf("%c", &salvar);
+
+    if (salvar == '\n') {
+        printf("Salvando as alteraçoes...\n");
+        fflush(arquivo); // Descarregar o buffer antes de fechar o arquivo
+    } else {
+        printf("Alteraçoes descartadas. Saindo sem salvar.\n");
+    }
+
+    // Fechar o arquivo
+    fclose(arquivo);
+}
+
+void exibirHistorico(const char *arquivo) {
+    FILE *arquivoCSV = fopen(arquivo, "r");
+
+    if (arquivoCSV == NULL) {
+        fprintf(stderr, "Erro ao abrir o arquivo %s\n", arquivo);
+        return;
+    }
+
+    char linha[TAMANHO_LINHA];
+
+    // Imprime o cabeçalho
+    if (fgets(linha, TAMANHO_LINHA, arquivoCSV) != NULL) {
+        printf("Historico de %s:\n", arquivo);
+        printf("%s", linha);
+    }
+
+    // Imprime as linhas de dados
+    while (fgets(linha, TAMANHO_LINHA, arquivoCSV) != NULL) {
+        printf("%s", linha);
+    }
+
+    fclose(arquivoCSV);
+}
--- vrumvrum.c
+++ vrumvrum.c
@@ -8,7 +8,7 @@ int main() {
         printf("1. Comprar\n");          
         printf("2. Venda\n");
         printf("3. Alterar Dados\n");
-        printf("4. Backup\n");
+        printf("4. Exibir Compras e Vendas\n");
         printf("5. Sair\n");
         printf("Escolha uma opcao: ");
         scanf("%d", &opcao);
@@ -17,117 +17,118 @@ int main() {
             case 1: { //compra carro
 
                 Carro carroCompra;
-/*
-                printf("Digite os dados do carro que deseja comprar:\n");
-                printf("Preco: ");
-                scanf("%d", &carroCompra.preco);
-                printf("Ano: ");
-                scanf("%d", &carroCompra.ano);
-                printf("Marca: ");
-                scanf("%s", carroCompra.marca);
-                printf("Modelo: ");
-                scanf("%s", carroCompra.modelo);
-                printf("Condicao: ");
-                scanf("%s", carroCompra.condicao);
-                printf("Combustivel: ");
-                scanf("%s", carroCompra.combustivel);
-                printf("Odometro: ");
-                scanf("%d", &carroCompra.odometro);
-                printf("Status: ");
-                scanf("%s", carroCompra.status);
-                printf("Cambio: ");
-                scanf("%s", carroCompra.cambio);
-                printf("Tamanho: ");
-                scanf("%s", carroCompra.tamanho);
-                printf("Tipo: ");
-                scanf("%s", carroCompra.tipo);
-                printf("Cor: ");
-                scanf("%s", carroCompra.cor);
-                */
-                carroCompra.preco = 1000;
-                carroCompra.ano = 1997;
-                strcpy(carroCompra.marca, "acura");
-                strcpy(carroCompra.modelo, "3.0cl");
-                strcpy(carroCompra.condicao,"razoavel");
-                strcpy(carroCompra.combustivel,"gasolina");
-                carroCompra.odometro = 300000;
-                strcpy(carroCompra.status,"desconhecido");
-                strcpy(carroCompra.cambio,"automatico");
-                strcpy(carroCompra.tamanho,"compacto");
-                strcpy(carroCompra.tipo,"coupe");
-                strcpy(carroCompra.cor, "vermelho");
 
+             printf("Digite os dados do carro que deseja comprar:\n");
+    printf("Preco: ");
+    scanf("%d", &carroCompra.preco);
 
+    printf("Ano: ");
+    scanf("%d", &carroCompra.ano);
+
+    printf("Marca: ");
+    scanf(" %[^\n]s", carroCompra.marca);
+
+    printf("Modelo: ");
+    scanf(" %[^\n]s", carroCompra.modelo);
+
+    printf("Condicao: ");
+    scanf(" %[^\n]s", carroCompra.condicao);
+
+    printf("Combustivel: ");
+    scanf(" %[^\n]s", carroCompra.combustivel);
+
+    printf("Odometro: ");
+    scanf("%d", &carroCompra.odometro);
+
+    printf("Status: ");
+    scanf(" %[^\n]s", carroCompra.status);
+
+    printf("Cambio: ");
+    scanf(" %[^\n]s", carroCompra.cambio);
+
+    printf("Tamanho: ");
+    scanf(" %[^\n]s", carroCompra.tamanho);
+
+    printf("Tipo: ");
+    scanf(" %[^\n]s", carroCompra.tipo);
+
+    printf("Cor: ");
+    scanf(" %[^\n]s", carroCompra.cor);
 
                 marcasAdiciona(carroCompra.marca);
                 remover_linha_arquivo(OFERTAS, Criarbuffer(carroCompra));
                 adicionar_linha_arquivo(ESTOQUE, Criarbuffer(carroCompra));
                 adicionar_linha_arquivo(HISTORICO_COMPRAS, Criarbuffer(carroCompra));
 
-                 printf("Carro comprado com sucesso!\n");
+                printf("Carro comprado com sucesso!\n");
 
                 break;
             }
             case 2: { //venda carro
 
                  Carro  carroVenda;
+            printf("Digite os dados do carro que deseja vender:\n");
+            printf("Preco: ");
+            scanf("%d", &carroVenda.preco);
 
-                carroVenda.preco = 1000;
-                carroVenda.ano = 1997;
-                strcpy( carroVenda.marca, "acura");
-                strcpy( carroVenda.modelo, "3.0cl");
-                strcpy( carroVenda.condicao,"razoavel");
-                strcpy( carroVenda.combustivel,"gasolina");
-                carroVenda.odometro = 300000;
-                strcpy( carroVenda.status,"desconhecido");
-                strcpy( carroVenda.cambio,"automatico");
-                strcpy( carroVenda.tamanho,"compacto");
-                strcpy( carroVenda.tipo,"coupe");
-                strcpy( carroVenda.cor, "vermelho");
+            printf("Ano: ");
+            scanf("%d", &carroVenda.ano);
 
-                
+            printf("Marca: ");
+            scanf(" %[^\n]s", carroVenda.marca);
+
+            printf("Modelo: ");
+            scanf(" %[^\n]s", carroVenda.modelo);
+
+            printf("Condicao: ");
+            scanf(" %[^\n]s", carroVenda.condicao);
+
+            printf("Combustivel: ");
+            scanf(" %[^\n]s", carroVenda.combustivel);
+
+            printf("Odometro: ");
+            scanf("%d", &carroVenda.odometro);
+
+            printf("Status: ");
+            scanf(" %[^\n]s", carroVenda.status);
+
+            printf("Cambio: ");
+            scanf(" %[^\n]s", carroVenda.cambio);
+
+            printf("Tamanho: ");
+            scanf(" %[^\n]s", carroVenda.tamanho);
+
+            printf("Tipo: ");
+            scanf(" %[^\n]s", carroVenda.tipo);
+
+            printf("Cor: ");
+            scanf(" %[^\n]s", carroVenda.cor);
+
                 remover_linha_arquivo(ESTOQUE, Criarbuffer(carroVenda));
                 adicionar_linha_arquivo(HISTORICO_VENDAS, Criarbuffer(carroVenda));
+                removerLinhaPorMarca(ESTOQUE, MARCAS);
 
-                 char marcaDoBuffer[TAMANHO_LINHA];
+                printf("Carro vendido com sucesso!\n");                
 
-                 sscanf(Criarbuffer(carroVenda), "%*[^,],%*[^,],%[^,]", marcaDoBuffer);  
-
-
-                 printf("Carro vendido com sucesso!\n");                
-
-                
                 break;
             }
             case 3: { //alterar dados
-                 int escolha;
-                      printf("Escolha o arquivo que deseja alterar\n");
-                      printf("1 = Estoque\n");
-                      printf("2 = Lista de Ofertas\n");
-
-                 scanf("%d", &escolha);
-
-
-                if(escolha==1){}
-                else if(escolha==2){}
-
+                 editarArquivoCSV();
                  break;
-                }
-            case 4: { //backup
-                        break;
+            }    
+            case 4: { //exibir historico vendas e compras
+                 exibirHistorico(HISTORICO_COMPRAS);
+                 exibirHistorico(HISTORICO_VENDAS);
+                 break;
             } 
-            case 5: { //sair
-                        break;
-            } 
-            
-                printf("Saindo do programa...\n");
+            case 5: {  
+             printf("Saindo do programa...\n");
                 break;
-
+             }
             default:
                 printf("Opcao invalida.\n");
         }
-
     } while (opcao != 5);
-
     return 0;
 }
+
